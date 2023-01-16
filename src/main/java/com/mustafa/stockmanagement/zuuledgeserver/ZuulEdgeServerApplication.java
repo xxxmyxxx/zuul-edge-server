becde301a70baf68477a4bba9b3870f01fb62ae3
Mustafa <yazilimcimiyim@gmail.com>
2023-01-17 00:01:48 +0300

    1

---
+++ src/main/java/com/mustafa/stockmanagement/zuuledgeserver/ZuulEdgeServerApplication.java
@@ -0,0 +1,21 @@
+package com.mustafa.stockmanagement.zuuledgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@EnableDiscoveryClient
+@EnableEurekaClient
+@SpringBootApplication
+@EnableZuulProxy
+@CrossOrigin(origins ="*",maxAge = 3600)
+public class ZuulEdgeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulEdgeServerApplication.class, args);
+    }
+
+}
